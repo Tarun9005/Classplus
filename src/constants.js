f5fded181453dde9efab537e099f47ac4b392094
Tarun9005 <shekhertarun@gmail.com>
2021-06-25 16:01:43 +0530

    work done

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const constants = {
+	BASE_URL:
+		"https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=cd9017a589f2afbdd8da19d79c79578d&per_page=20",
+	STORAGE_KEY: "flicker_search_queries"
+};
+
+export default constants;
